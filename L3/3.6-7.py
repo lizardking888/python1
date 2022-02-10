bf8d87056c261eb5bce56a023f816f2826246136
Dingo <limitlessquantum888@gmail.com>
2022-02-10 11:25:21 +0300

    ДЗ. третий урок
    с пятой задачей не удалось разобраться. 
    решение шестой задачи подходит для первого и второго варианта задачи

---
+++ L3/3.6-7.py
@@ -0,0 +1,10 @@
+line = input ('Введите строку : ')
+def int_func(line):
+    line1 = line.title()
+    return line1
+
+print (int_func(line))
+
+#Решение подходит как для первого варианта, так и для второго    
+        
+    
